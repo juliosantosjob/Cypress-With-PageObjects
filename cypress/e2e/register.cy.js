a02cd18e50a5a815601cb8111622acbc8bb8ef2e
Julio Santos <julioc.9730@gmail.com>
2024-07-17 22:45:11 -0300

    Atualizar o register.cy.js

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -2,46 +2,43 @@ import RegisterPage from '../pages/register.page'
 import HomePage from '../pages/home.page'
 
 describe('Cadastro', () => {
-  const homePage = new HomePage()
-  const registerPage = new RegisterPage()
-
   it('Cadastro de ponto de doação com sucesso', () => { 
-    homePage.goToHome()
-    registerPage.accessRegisterPage()
-    registerPage.fillForm('Praça 08', 'test@example.com', '07144000', '2000', 'Rua dos bobos')
-    registerPage.submitForm()
-    registerPage.verifyRegisterDonation('Você fez a diferença!')
+    HomePage.goToHome()
+    RegisterPage.accessRegisterPage()
+    RegisterPage.fillForm('Praça 08', 'test@example.com', '07144000', '2000', 'Rua dos bobos')
+    RegisterPage.submitForm()
+    RegisterPage.verifyRegisterDonation('Você fez a diferença!')
   })
 
   it('Não deve realizar cadastro com email invalido', () => {
-    homePage.goToHome()
-    registerPage.accessRegisterPage()
-    registerPage.fillForm('Praça 08', 'testexample.com', '07144000', '2000', 'Rua dos bobos')
-    registerPage.submitForm()
-    registerPage.verifyError('Informe um email válido')
+    HomePage.goToHome()
+    RegisterPage.accessRegisterPage()
+    RegisterPage.fillForm('Praça 08', 'testexample.com', '07144000', '2000', 'Rua dos bobos')
+    RegisterPage.submitForm()
+    RegisterPage.verifyError('Informe um email válido')
   })
 
   it('Não deve realizar cadastro com cep inválido', () => {
-    homePage.goToHome()
-    registerPage.accessRegisterPage()
-    registerPage.fillForm('Praça 08', 'test@example.com', '0000', '2000', 'Rua dos bobos')
-    registerPage.verifyError('Informe um CEP válido')
+    HomePage.goToHome()
+    RegisterPage.accessRegisterPage()
+    RegisterPage.fillForm('Praça 08', 'test@example.com', '0000', '2000', 'Rua dos bobos')
+    RegisterPage.verifyError('Informe um CEP válido')
   })
 
   it('Não deve realizar cadastro com campos obrigatórios em branco', () => {
-    homePage.goToHome()
-    registerPage.accessRegisterPage()
-    registerPage.submitForm()
+    HomePage.goToHome()
+    RegisterPage.accessRegisterPage()
+    RegisterPage.submitForm()
 
-    const messagesErro = [
+    const messages = [
       'Informe o seu nome completo',
       'Informe o seu melhor email',
       'Informe o seu CEP',
       'Informe um número maior que zero'
     ]
 
-    for (let output = 0; output < messagesErro.length; output++) {
-      registerPage.verifyError(messagesErro[output])
+    for (const output : messages) {
+      RegisterPage.verifyError(output)
     }
   })
 })
