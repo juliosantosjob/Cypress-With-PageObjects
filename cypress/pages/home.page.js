535395e7a9f77131e4fc61bb6a853e3907b66ad0
Julio Santos <julioc.9730@gmail.com>
2024-07-20 16:55:50 -0300

    Atualizar o home.page.js

--- cypress/pages/home.page.js
+++ cypress/pages/home.page.js
@@ -2,7 +2,7 @@ import { ELM_HOME } from "../elements/home.element"
 
 class HomePage {
 
-    goToHome() {
+    openHome() {
         cy.visit('/')
         cy.get(ELM_HOME.tagTitle).should('have.text',
             'Conectando corações, mudando vidas!')
