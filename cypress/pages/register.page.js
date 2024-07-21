a7ee8b4eed615bf744dd09e7fa86013018295d79
Julio Santos <julioc.9730@gmail.com>
2024-07-21 20:05:14 -0300

    Atualizar o register.page.js

--- cypress/pages/register.page.js
+++ cypress/pages/register.page.js
@@ -5,7 +5,7 @@ class RegisterPage {
 
     openRegisterPage() {
         cy.visit('/signup')
-        cy.get(ELM_HOME.buttonRegistDonation).click()
+        cy.get(ELM_REGISTER.buttonRegistDonation).click()
         cy.get(ELM_HOME.tagTitle).should('have.text',
             'Cadastro de ponto de doação')
     }
